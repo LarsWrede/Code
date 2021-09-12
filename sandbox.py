c334eb89090559016ab4a5a24272a1cd4f8bf632
Philipp Voit <voitpg@gmail.com>
2021-09-12 19:53:22 +0200

    Factorial script

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,9 @@
-j = 2
+# factorial
+
+def calc_factorial(n):
+    return 1 if (n == 1 or n == 0) else n * calc_factorial(n - 1)
+
+
+num = 5
+calc_factorial(num)
+print("Factorial of", num, "is", calc_factorial(num))
